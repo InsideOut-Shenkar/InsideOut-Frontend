6b6053e8e86f8449bd0635ca375c064c322cc8d4
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-05-24 12:37:06 +0300

    get medical data api hook

---
+++ src/api/useGetMedData.js
@@ -0,0 +1,22 @@
+// ==============================|| GET PATIENTS HOOK ||============================== //
+
+const useGetMedData = () => {
+  const getMedData = async (id) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_ENDPOINT}/medical-data/${id}`);
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.details ? `${data.details}, details: ${data.details}` : data.error || 'Unknown error occurred');
+      }
+      return updatedJson;
+    } catch (err) {
+      console.error('Failed to fetch patient data:', err);
+      throw new Error(`Data fetching failed, details: ${err.message}`);
+    }
+  };
+
+  return { getMedData };
+};
+
+export default useGetMedData;
