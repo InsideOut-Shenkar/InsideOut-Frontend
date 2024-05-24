c65d91a20c4cebed01166a529c390af62542394e
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-05-24 12:53:39 +0300

    Export API hooks for patient and medical data management.

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+// project import
+import useGetPatients from './useGetPatients';
+import useDeletePatients from './useDeletePatients';
+import useAddPatient from './useAddPatient';
+import useAddMedicalData from './useAddMedicalData';
+import useGetPatient from './useGetPatient';
+import useGetMedData from './useGetMedData';
+
+// ==============================|| API HOOKS ||============================== //
+
+export { useGetPatients, useDeletePatients, useAddPatient, useAddMedicalData, useGetPatient, useGetMedData };
