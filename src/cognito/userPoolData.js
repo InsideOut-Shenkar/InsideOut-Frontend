1112f7baa6453d67cc6b2f884aadf7a954cf81da
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-05-24 11:50:57 +0300

    amazon cognito user pool coordinates

---
+++ src/cognito/userPoolData.js
@@ -0,0 +1,8 @@
+// ============================|| AMAZON COGNITO - USER POOL DATA ||============================ //
+
+const userPoolData = {
+  UserPoolId: process.env.REACT_APP_USER_POOL,
+  ClientId: process.env.REACT_APP_CLIENT_ID
+};
+
+export default userPoolData;
