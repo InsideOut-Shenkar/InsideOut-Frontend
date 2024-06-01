f9bf92407aab0e7d5837ff8ed58f291889ed2fd0
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-01 22:50:37 +0300

    Add Snackbar for password update notifications and enhance login form with new password mode.

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
+
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Grid, Stack, Typography, Snackbar, Button, Alert } from '@mui/material';
 
 // project import
 import AuthLogin from './auth-forms/AuthLogin';
@@ -7,19 +9,46 @@ import AuthWrapper from './AuthWrapper';
 
 // ================================|| LOGIN ||================================ //
 
-const Login = () => (
-  <AuthWrapper>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-          <Typography variant="h3">Login</Typography>
-        </Stack>
+const Login = () => {
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [formMode, setFormMode] = useState('login'); // 'login' or 'newPassword'
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  const handleNewPassword = () => {
+    setSnackbarOpen(false);
+    setFormMode(formMode === 'login' ? 'newPassword' : 'login');
+  };
+
+  return (
+    <AuthWrapper>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+            <Typography variant="h3">{formMode === 'login' ? 'Login' : 'Update Password'}</Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <AuthLogin setSnackbarOpen={setSnackbarOpen} formMode={formMode} />
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <AuthLogin />
-      </Grid>
-    </Grid>
-  </AuthWrapper>
-);
+      <Snackbar open={snackbarOpen} onClose={handleSnackbarClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={formMode === 'login' ? 'warning' : 'success'}
+          action={
+            <Button color="inherit" size="small" onClick={handleNewPassword}>
+              {formMode === 'login' ? 'Set New Password' : 'Go To Login'}
+            </Button>
+          }
+        >
+          {formMode === 'login' ? 'Make a new password' : 'New password is set'}
+        </Alert>
+      </Snackbar>
+    </AuthWrapper>
+  );
+};
 
 export default Login;
