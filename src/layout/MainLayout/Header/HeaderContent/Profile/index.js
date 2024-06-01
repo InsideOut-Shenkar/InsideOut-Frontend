b901fe437662e6c3402ee6f22395545a6772bfe7
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-01 22:45:25 +0300

    Enhance Profile component to display current user's name and role.

--- src/layout/MainLayout/Header/HeaderContent/Profile/index.js
+++ src/layout/MainLayout/Header/HeaderContent/Profile/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
@@ -20,22 +20,23 @@ import {
 } from '@mui/material';
 
 // project import
-import MainCard from 'components/MainCard';
-import Transitions from 'components/@extended/Transitions';
+import useLogout from 'utils/auth';
 import ProfileTab from './ProfileTab';
+import MainCard from 'components/MainCard';
+import getUserRoles from 'utils/getUserRoles';
+import Transitions from 'components/@extended/Transitions';
+import getCurrentUserAttributes from 'utils/aws/cognito/getCurrentUserAttributes';
 
 // assets
 import avatar from 'assets/images/users/doctor.png';
 import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 
 // tab panel wrapper
-function TabPanel({ children, value, index, ...other }) {
-  return (
-    <div role="tabpanel" hidden={value !== index} id={`profile-tabpanel-${index}`} aria-labelledby={`profile-tab-${index}`} {...other}>
-      {value === index && children}
-    </div>
-  );
-}
+const TabPanel = ({ children, value, index, ...other }) => (
+  <div role="tabpanel" hidden={value !== index} id={`profile-tabpanel-${index}`} aria-labelledby={`profile-tab-${index}`} {...other}>
+    {value === index && children}
+  </div>
+);
 
 TabPanel.propTypes = {
   children: PropTypes.node,
@@ -43,24 +44,40 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired
 };
 
-function a11yProps(index) {
-  return {
-    id: `profile-tab-${index}`,
-    'aria-controls': `profile-tabpanel-${index}`
-  };
-}
+const a11yProps = (index) => ({
+  id: `profile-tab-${index}`,
+  'aria-controls': `profile-tabpanel-${index}`
+});
+
+/**
+ * Convert a role to its abbreviation
+ * @param {string} role - The role to be converted
+ * @returns {string} - The abbreviation of the role
+ */
+const roleToAbbreviation = (role) => {
+  switch (role) {
+    case 'Admin':
+      return 'Adm.';
+    case 'Doctor':
+      return 'Dr.';
+    case 'User':
+      return 'Usr.';
+    default:
+      return '';
+  }
+};
 
 // ==============================|| HEADER CONTENT - PROFILE ||============================== //
 
 const Profile = () => {
   const theme = useTheme();
-
-  const handleLogout = async () => {
-    // logout
-  };
-
+  const logout = useLogout();
   const anchorRef = useRef(null);
+  const [value, setValue] = useState(0);
   const [open, setOpen] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [userRole, setUserRole] = useState('');
+
   const handleToggle = () => {
     setOpen((prevOpen) => !prevOpen);
   };
@@ -72,13 +89,26 @@ const Profile = () => {
     setOpen(false);
   };
 
-  const [value, setValue] = useState(0);
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   const iconBackColorOpen = 'grey.300';
+
+  useEffect(() => {
+    const fetchUserAttributes = async () => {
+      try {
+        const userAttributes = await getCurrentUserAttributes();
+        const userRoles = await getUserRoles();
+        setUserName(userAttributes.name);
+        setUserRole(userRoles ? userRoles[0] : null);
+      } catch (error) {
+        console.error('Error fetching user attributes:', error);
+      }
+    };
+
+    fetchUserAttributes();
+  }, []);
 
   return (
     <Box sx={{ flexShrink: 0, ml: 0.75 }}>
@@ -98,7 +128,9 @@ const Profile = () => {
       >
         <Stack direction="row" spacing={2} alignItems="center" sx={{ p: 0.5 }}>
           <Avatar alt="profile user" src={avatar} sx={{ width: 32, height: 32 }} />
-          <Typography variant="subtitle1">Dr. Clara Wilson</Typography>
+          <Typography variant="subtitle1">
+            {roleToAbbreviation(userRole)} {userName}
+          </Typography>
         </Stack>
       </ButtonBase>
       <Popper
@@ -141,15 +173,15 @@ const Profile = () => {
                           <Stack direction="row" spacing={1.25} alignItems="center">
                             <Avatar alt="profile user" src={avatar} sx={{ width: 32, height: 32 }} />
                             <Stack>
-                              <Typography variant="h6">John Doe</Typography>
+                              <Typography variant="h6">{userName}</Typography>
                               <Typography variant="body2" color="textSecondary">
-                                Doctor
+                                {userRole}
                               </Typography>
                             </Stack>
                           </Stack>
                         </Grid>
                         <Grid item>
-                          <IconButton size="large" color="secondary" onClick={handleLogout}>
+                          <IconButton size="large" color="secondary" onClick={logout}>
                             <LogoutOutlined />
                           </IconButton>
                         </Grid>
@@ -186,7 +218,7 @@ const Profile = () => {
                           </Tabs>
                         </Box>
                         <TabPanel value={value} index={0} dir={theme.direction}>
-                          <ProfileTab handleLogout={handleLogout} />
+                          <ProfileTab handleLogout={logout} />
                         </TabPanel>
                       </>
                     )}
