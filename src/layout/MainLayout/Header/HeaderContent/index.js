7018eb0b491b032140a7a7c1ed246d5d90133f9a
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-01 22:44:43 +0300

    Add color mode toggle functionality in HeaderContent.

--- src/layout/MainLayout/Header/HeaderContent/index.js
+++ src/layout/MainLayout/Header/HeaderContent/index.js
@@ -1,22 +1,41 @@
+import { useContext } from 'react';
+
 // material-ui
 import { Box, IconButton, useMediaQuery } from '@mui/material';
-import { MoonOutlined } from '@ant-design/icons';
+import { useTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 // project import
 import Profile from './Profile';
 import MobileSection from './MobileSection';
+import ColorModeContext from 'utils/contexts/colorModeContext';
 
 // ==============================|| HEADER - CONTENT ||============================== //
 
 const HeaderContent = () => {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
   const matchesXs = useMediaQuery((theme) => theme.breakpoints.down('md'));
 
   return (
     <>
       {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
-
-      <IconButton disableRipple color="secondary" sx={{ color: 'text.primary', bgcolor: 'grey.100' }}>
-        <MoonOutlined />
+      <IconButton
+        color="secondary"
+        sx={{
+          color: 'text.primary',
+          ml: 1,
+          '&:hover': {
+            bgcolor: 'grey.100',
+            '& svg': {
+              color: 'black'
+            }
+          }
+        }}
+        onClick={colorMode.toggleColorMode}
+      >
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon sx={{ color: 'white' }} />}
       </IconButton>
       {!matchesXs && <Profile />}
       {matchesXs && <MobileSection />}
