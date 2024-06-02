a14c62d7036eed0f7966d40e790d9083a4781057
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-03 01:24:22 +0300

    Add ErrorRoutes to ThemeRoutes for handling error pages.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,10 @@ import { useRoutes } from 'react-router-dom';
 import MainRoutes from './MainRoutes';
 import LoginRoutes from './LoginRoutes';
 import AdminRoutes from './AdminRoutes';
+import ErrorRoutes from './ErrorRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-  return useRoutes([MainRoutes, LoginRoutes, AdminRoutes]);
+  return useRoutes([MainRoutes, LoginRoutes, AdminRoutes, ErrorRoutes]);
 }
