c27b55e660732e5661d0ad652035008a61ef7f9d
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-03 01:23:19 +0300

    Add dark mode support to Empty component with different GIF for dark theme.

--- src/assets/images/error/empty/index.js
+++ src/assets/images/error/empty/index.js
@@ -1,13 +1,19 @@
 // material-ui
 import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+// assets
 import NotFoundGIF from './empty.gif';
+import DarkNotFoundGIF from './empty-dark.gif'
 
 // ==============================|| NOT FOUND GIF ||============================== //
 
 const Empty = ({ width, height }) => {
+  const theme = useTheme();
+
   return (
     <Box sx={{ width: width, height: height }}>
-      <img src={NotFoundGIF} alt="Not Found" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+      <img src={theme.palette.mode === 'dark' ? DarkNotFoundGIF : NotFoundGIF} alt="Not Found" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
     </Box>
   );
 };
